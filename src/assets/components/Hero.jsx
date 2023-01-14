b62ddb714a942c73f580e3f0e6c5c85da81b1bb8
DainiusMI <mental.def@gmail.com>
2023-01-14 21:02:40 +0200

    after big file delete

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -3,42 +3,6 @@ import DynamicSection from "./DynamicSection";
 import mainDataJSON from "../mainData.json"
 
 const heroDOM = mainDataJSON.hero.data
-/*
-const heroData = [
-    {   
-        id: 1,
-        title: "PACK A SURPIZE THIS SEASON",
-        text: "The BMW Road Home Sales Event offers a credit of up to $3.250 on select BMW models - now through Janury 3rd",
-        buttons: ["Offers Details", "Build Your Own"],
-        slogan: [],
-        isActive: true
-    },
-    {   
-        id: 2,
-        title: "EMBODY THE MAXIMUM",
-        text: "THE FIRST EVER BMW XM",
-        buttons: ["Learn More", "Pre-Order"],
-        slogan: ["The", "Ultimate", "Driving Machine ©"],
-        isActive: false
-    },
-    {   
-        id: 3,
-        title: "AS ELECTRIC AS YOU",
-        text: "iX shown on left, i4 shown on the right",
-        buttons: ["Explore the iX", "Explore the i4"],
-        slogan: ["The Ultimate", "Electric", "Driving Machine"],
-        isActive: false
-    },
-    {   
-        id: 4,
-        title: "THE M2",
-        text: "DISCOVER THE 2023 BMW M2 COUPE",
-        buttons: ["Build Yours", "Learn More"],
-        slogan: ["The", "Ultimate", "Driving Machine ©"],
-        isActive: false
-    }
-]
-*/
 
 export default function Hero({screenSize}) {
 
@@ -65,43 +29,3 @@ export default function Hero({screenSize}) {
         />
     )
 }
-
-
-
-
-
-/*    return (    
-            <div 
-                key={hero.id}
-                className={`hero hero__${hero.id}`}
-            >
-            <div className="hero__left">
-                <h1 className="hero__title">{hero.title}</h1>
-                <p className="hero__text">{hero.text}</p>
-                <div className="hero__buttons">
-                    {
-                        hero.buttons.map((button, idx) => {
-                            return <button key={idx}>{button}</button>
-                        })
-                    }
-                </div>
-            </div>
-
-            {hero.slogan.length > 0 && 
-                <div className="hero__right">
-                {
-                    hero.slogan.map((slogan, idx) => {
-                        return <p key={idx} className="slogan__text">{slogan}</p>
-                    })
-                }
-                </div>
-            }
-
-            <Selector 
-                state={heroState}
-                setState={setHeroState}
-                active={activeHero}
-                setActive={setActiveHero}
-            />
-        </div>
-    )*/
--- src/assets/components/Services.jsx
+++ src/assets/components/Services.jsx
@@ -4,42 +4,6 @@ import mainDataJSON from "../mainData.json"
 
 const servicesJSON = mainDataJSON.services.data
 
-/*
-const servicesData = [
-    {   
-        id: 1,
-        title: "DOWNLOAD THE NEW MY BMW APP",
-        text: "",
-        isActive: true
-    },
-    {   
-        id: 2,
-        title: "INTRODUCING BMW ULTIMATE CARE OIL SERVICES",
-        text: "3 yaers prepaid for $199 for BMWs that have reached 60 months or 60.000 miles.",
-        isActive: false
-    },
-    {   
-        id: 3,
-        title: "DESIGNED FOR THE DRIVEN",
-        text: "Ear up to 5X POINTS on eligible purchases.",
-        isActive: false
-    },
-    {   
-        id: 4,
-        title: "BMW VALUE SERVICE",
-        text: "Think we're expensive? Think again.",
-        isActive: false
-    },
-    {   
-        id: 5,
-        title: "ORIGINAL BMW TIRES",
-        text: "Take control of your ride.",
-        isActive: false
-    }
-]
-*/
-
-
 export default function Services({screenSize}) {
     const [serviceState, setServiceState] = useState(servicesJSON)
     const [activeService, setActiveService] = useState({
@@ -62,29 +26,3 @@ export default function Services({screenSize}) {
         />
     )
 }
-
-
-/*
-
-
-    return (
-        <div 
-            className="services"
-            id={`service-${service.id}`}    
-        >
-            <p className="service__title">{service.title}</p>
-            {
-                service.text !== "" && <p className="service__text">{service.text}</p>
-            }
-            <button>Learn More</button>
-
-            <Selector 
-                state={serviceState}
-                setState={setServiceState}
-                active={activeService}
-                setActive={setActiveService}
-            />
-        </div>
-    )
-
-    */
